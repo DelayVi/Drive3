4ac1309dccce16e3081f27e6fd38f3ac4e49b322
DelayVi <withfreaky@gmail.com>
2022-09-11 16:18:19 +0700

    Add picasso

--- app/src/main/java/ru/delayvi/drive3/presentation/car_fragment/CarFragment.kt
+++ app/src/main/java/ru/delayvi/drive3/presentation/car_fragment/CarFragment.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
+import com.squareup.picasso.Picasso
+import ru.delayvi.drive3.R
 import ru.delayvi.drive3.databinding.FragmentCarBinding
 import ru.delayvi.drive3.domain.entity.Car
 
@@ -52,6 +54,10 @@ class CarFragment : Fragment() {
     fun entryParams(car: Car){
         binding.etName.setText(car.brand)
         binding.etCount.setText(car.model)
+        Picasso.get()
+            .load(car.imageUri)
+            .placeholder(R.drawable.ic_launcher_background)
+            .into(binding.ivCar)
     }
 
 }
