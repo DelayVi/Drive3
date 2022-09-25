b973e34bd6395b83c4c6fdc098881b66f3b15d8c
DelayVi <withfreaky@gmail.com>
2022-09-25 14:05:08 +0700

    add Dagger

--- app/src/main/java/ru/delayvi/drive3/di/AppModule.kt
+++ app/src/main/java/ru/delayvi/drive3/di/AppModule.kt
@@ -1,7 +1,26 @@
 package ru.delayvi.drive3.di
 
 import dagger.Module
+import dagger.Provides
+import ru.delayvi.drive3.domain.usecases.AddCarUseCase
+import ru.delayvi.drive3.domain.usecases.EditCarUseCase
+import ru.delayvi.drive3.domain.usecases.GetCarUseCase
+import ru.delayvi.drive3.presentation.car_fragment.CarFragmentViewModel
+import ru.delayvi.drive3.presentation.main_fragment.MainViewModel
 
 @Module(includes = [AppBindsModule::class])
 class AppModule {
+
+    @Provides
+    fun provideCarFragmentViewModel(
+        addCarUseCase: AddCarUseCase,
+        editCarUseCase: EditCarUseCase,
+        getCarUseCase: GetCarUseCase
+    ): CarFragmentViewModel {
+        return CarFragmentViewModel(
+            addCarUseCase,
+            editCarUseCase,
+            getCarUseCase
+        )
+    }
 }
