e63d62a86294ae4dff25076ac23065897381aac4
DelayVi <withfreaky@gmail.com>
2022-09-25 14:07:37 +0700

    add Dagger

--- app/src/main/java/ru/delayvi/drive3/di/AppModule.kt
+++ app/src/main/java/ru/delayvi/drive3/di/AppModule.kt
@@ -2,14 +2,36 @@ package ru.delayvi.drive3.di
 
 import dagger.Module
 import dagger.Provides
-import ru.delayvi.drive3.domain.usecases.AddCarUseCase
-import ru.delayvi.drive3.domain.usecases.EditCarUseCase
-import ru.delayvi.drive3.domain.usecases.GetCarUseCase
+import ru.delayvi.drive3.domain.usecases.*
 import ru.delayvi.drive3.presentation.car_fragment.CarFragmentViewModel
+import ru.delayvi.drive3.presentation.car_fragment.CarFragmentViewModelFactory
 import ru.delayvi.drive3.presentation.main_fragment.MainViewModel
+import ru.delayvi.drive3.presentation.main_fragment.MainViewModelFactory
 
 @Module(includes = [AppBindsModule::class])
 class AppModule {
+
+    @Provides
+    fun provideMainViewModel(
+        getCarListUseCase: GetCarListUseCase,
+        deleteCarUseCase: DeleteCarUseCase
+    ): MainViewModel {
+        return MainViewModel(
+            getCarListUseCase,
+            deleteCarUseCase
+        )
+    }
+
+    @Provides
+    fun provideMainViewModelFactory(
+        getCarListUseCase: GetCarListUseCase,
+        deleteCarUseCase: DeleteCarUseCase
+    ): MainViewModelFactory {
+        return MainViewModelFactory(
+            getCarListUseCase,
+            deleteCarUseCase
+        )
+    }
 
     @Provides
     fun provideCarFragmentViewModel(
@@ -18,6 +40,19 @@ class AppModule {
         getCarUseCase: GetCarUseCase
     ): CarFragmentViewModel {
         return CarFragmentViewModel(
+            addCarUseCase,
+            editCarUseCase,
+            getCarUseCase
+        )
+    }
+
+    @Provides
+    fun provideCarFragmentViewModelFactory(
+        addCarUseCase: AddCarUseCase,
+        editCarUseCase: EditCarUseCase,
+        getCarUseCase: GetCarUseCase
+    ): CarFragmentViewModelFactory {
+        return CarFragmentViewModelFactory(
             addCarUseCase,
             editCarUseCase,
             getCarUseCase
